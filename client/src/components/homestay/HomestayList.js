69db0225c9dd6c0c8bf39fbc5ca2530186be32d2
Nico Meyer <nico.meyer@i22.de>
2020-06-10 15:53:06 +0200

    fix: fixes capacity select

--- client/src/components/homestay/HomestayList.js
+++ client/src/components/homestay/HomestayList.js
@@ -1,41 +1,115 @@
 import React, {  useEffect, useState } from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getHomestays } from '../../actions/homestay';
-
-const initialState = {
-    type: '',
-    capacity: '',
-    price: '',
-    minPrice: '',
-    maxPrice: '',
-    minSize: '',
-    maxSize: '',
-    breakfast: '',
-    pat: '',
-    searchPhrase: '',
-    // sortedhomestays: [],
-};
 
 const HomeStayList = ({
     getHomestays,
     homestay: {
       homestays,
-      capacity,
       sortedhomestays,
       loading
     }
   }) => {
-  const [ filteredHomestays, filterHomestays ] = useState([]);
-  const setFilteredHomestays = (homestays) => {
-    filterHomestays(homestays);
+  // state
+  const [ filteredHomestays, setFilteredHomestays ] = useState([]);
+
+  // selected room type
+  let [selectedType, setSelectedType] = useState('all');
+
+  // room types
+  const [types, setTypes] = useState([]);
+
+  // selected capacity
+  const [selectedCapacity, setSelectedCapacity] = useState('');
+
+  // capacities
+  let [capacities, setCapacities] = useState(null);
+
+  // set search phrase
+  const [searchPhrase, setSearchPhrase] = useState('');
+  // state end
+
+  // get all unique values
+  const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))];
+  };
+
+  useEffect(() => {
+    if (homestays.length < 1) {
+      getHomestays();
+    } else {
+      setFilteredHomestays(homestays);
+      console.log('initial on load', filteredHomestays);
+    }
+    // get types + add all + map to jsx and state
+    let uniqueTypes = getUnique(homestays, 'type');
+    uniqueTypes = ['all', ...uniqueTypes];
+
+    const mappedUniqueTypes = uniqueTypes.map((item, i) => {
+      return (
+      <option value={item} key={i}>{item}</option>
+      );
+    });
+
+    setTypes(mappedUniqueTypes);
+
+    // get capacities
+    capacities = getUnique(homestays, 'capacity');
+    capacities = [0, ...capacities];
+
+    capacities = capacities.map((item, i) => {
+      if (item === 0) {
+        return (
+          <option value={0} key={i}>Select Capacity</option>
+        );
+      }
+      return (
+        <option value={item} key={i}>{item}</option>
+      );
+    });
+
+    setCapacities(capacities);
+
+  }, [homestays]);
+
+  const handleChange = (e) => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value = e.target.value;
+    const isSearch = e.target.id === 'search';
+
+    console.log('type: ', type, 'name:', name, 'val:', value);
+
+    if (name === 'type') {
+      setSelectedType(value);
+    }
+    if (name === 'capacity' && value !== 0) {
+      setSelectedCapacity(parseInt(value));
+    }
+    if (isSearch) {
+      console.log('phrase', value);
+      setSearchPhrase(value);
+    }
   }
 
   useEffect(() => {
-    if (homestays.length < 1) getHomestays();
-    setFilteredHomestays(homestays);
-  }, [homestays]);
+    let tempHomestays = [...homestays];
 
+    if (selectedType !== 'all') {
+      tempHomestays = tempHomestays.filter(homestay => homestay.type === selectedType);
+    }
+
+    if (selectedCapacity !== 0) {
+      tempHomestays = tempHomestays.filter(homestay => homestay.capacity === selectedCapacity);
+    }
+
+    if (searchPhrase.length > 2) {
+      tempHomestays = tempHomestays.filter(homestay => homestay.name.includes(searchPhrase));
+    }
+
+    setFilteredHomestays(tempHomestays);
+  }, [selectedType, selectedCapacity, searchPhrase]);
 
   // homestay list-items
   const listItems = filteredHomestays.map((homestay) =>
@@ -44,19 +118,42 @@ const HomeStayList = ({
     </li>
   );
 
-  // search
-  const searchHomestay = (phrase) => {
-      const filteredHomestays = homestays.filter(homestay => homestay.name.toLowerCase().includes(phrase.toLowerCase()));
-      setFilteredHomestays(filteredHomestays);
-  }
-
   return (
     <div>
-      <div className="homestay-list__search">
+      <div className="filter-container">
+        <form className="form-group">
+          <div className="homestay-list__search">
         <input
+          id="search"
           type="text"
           placeholder="Search Homestay"
-          onChange={event => searchHomestay(event.target.value)} />
+          onChange={event => handleChange(event)} />
+      </div>
+        </form>
+        <form className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={selectedType}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </form>
+        <form className="form-group">
+          <label htmlFor="type">capacity</label>
+          <select
+            name="capacity"
+            id="capacity"
+            value={selectedCapacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {capacities}
+          </select>
+        </form>
       </div>
       <ul className="homestay-list">
         {listItems}
